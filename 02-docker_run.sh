98dbc14940383bbd000f1adc616a8f511181e3f5
Shigenori Suzuki <s.suzuki@gmail.com>
2025-05-10 22:30:54 +0900

    modified 02-docker_run.sh

--- 02-docker_run.sh
+++ 02-docker_run.sh
@@ -1,2 +1,24 @@
 #!/bin/zsh
-docker run -p 8888:8888 -v ${PWD}:/app polars-data-analysis
+# 初期値として現在のディレクトリを設定
+HOST_DIR=${PWD}
+
+# オプション解析
+while getopts "d:" opt; do
+  case $opt in
+    d)
+      HOST_DIR=$OPTARG
+      # 指定されたディレクトリが存在するか確認
+      if [ ! -d "$HOST_DIR" ]; then
+        echo "エラー: 指定されたディレクトリ '$HOST_DIR' は存在しません"
+        exit 1
+      fi
+      ;;
+    \?)
+      echo "使用法: $0 [-d ホストディレクトリのパス]"
+      exit 1
+      ;;
+  esac
+done
+
+echo "マウントするホストディレクトリ: $HOST_DIR"
+docker run -p 8888:8888 -v ${HOST_DIR}:/app polars-data-analysis
